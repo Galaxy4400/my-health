14c242c03d92fe10d4344ca610ab93b4f0b1c1f9
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-02-28 16:25:31 +0300

    commit

--- src/widgets/measure-btn/measure-btn.ui.tsx
+++ src/widgets/measure-btn/measure-btn.ui.tsx
@@ -17,11 +17,12 @@ export const MeasureBtn = ({ action, onSuccess, onError, nextStep, startCount = 
 	const [isRunning, setIsRunning] = useState(false);
 	const [isActionProcess, setIsActionProcess] = useState(false);
 	const [isComplete, setIsComplete] = useState(false);
+	const [isBtnClose, setIsBtnClose] = useState(false);
 	const [count, setCount] = useState(startCount);
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		if (count > 0) return;
+		if (!isRunning || count > 0 || isActionProcess) return;
 
 		setIsActionProcess(true);
 
@@ -36,14 +37,23 @@ export const MeasureBtn = ({ action, onSuccess, onError, nextStep, startCount = 
 				setIsComplete(true);
 				setIsRunning(false);
 				setIsActionProcess(false);
+
+				setTimeout(() => {
+					setIsBtnClose(false);
+				}, 500);
 			});
-	}, [count, action, onError, onSuccess]);
+	}, [count, action, onError, onSuccess, isRunning, isActionProcess]);
 
 	useEffect(() => {
 		if (!isRunning) return;
 
 		const interval = setInterval(() => {
 			setCount((prev) => {
+				if (prev <= 2) {
+					setTimeout(() => {
+						setIsBtnClose(true);
+					}, 500);
+				}
 				if (prev <= 1) {
 					clearInterval(interval);
 					return 0;
@@ -56,6 +66,8 @@ export const MeasureBtn = ({ action, onSuccess, onError, nextStep, startCount = 
 	}, [isRunning]);
 
 	const clickHandler = () => {
+		if (isRunning) return;
+
 		if (isComplete) {
 			navigate(nextStep);
 			return;
@@ -65,19 +77,21 @@ export const MeasureBtn = ({ action, onSuccess, onError, nextStep, startCount = 
 		setCount(startCount);
 	};
 
+	const btnText = isRunning ? count : isComplete ? 'Следующий шаг' : 'Измерить';
+	const btnCloseClass = isBtnClose ? 'close' : '';
+	const completeClass = isComplete ? 'complete' : '';
+	const activeClass = isRunning ? 'active' : '';
+	const processClass = isActionProcess ? 'process' : '';
+
 	return (
 		<div className={css['main']}>
-			<button className={cn(css['btn'], isComplete ? 'complete' : '')} onClick={clickHandler}>
-				{isComplete ? 'Следующий шаг' : 'Измерить'}
+			<button className={cn(css['btn'], completeClass, activeClass, btnCloseClass)} onClick={clickHandler}>
+				{btnText}
 			</button>
-			<div className={cn(css['loader'], isRunning ? 'active' : '')}>
-				<div className={cn(css['icon-wrapper'], isRunning ? 'active' : '')}>
-					<Icon className={cn(css['icon'], isRunning ? 'active' : '')} name={Icons.load} />
-					{isActionProcess ? (
-						<span className={css['processs']}>Измеряем...</span>
-					) : (
-						<span className={css['counter']}>{count}</span>
-					)}
+			<div className={cn(css['loader'], processClass)}>
+				<div className={cn(css['icon-wrapper'], processClass)}>
+					<Icon className={cn(css['icon'], processClass)} name={Icons.load} />
+					<span className={css['process']}>Измеряем...</span>
 				</div>
 			</div>
 		</div>
