581bc19e4b6da7dfaf3491ef46c4d50f15e15441
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-03-03 15:54:30 +0300

    commit

--- src/pages/cardio/ui/cardio-page.ui.tsx
+++ src/pages/cardio/ui/cardio-page.ui.tsx
@@ -4,8 +4,9 @@ import { Button, Container, MeasureStatus, PageHead, Steps, TitleBlock } from 's
 import { path } from 'shared/lib/router';
 import { useState } from 'react';
 import { useMeasure } from '../lib';
-
 import { Measure } from 'widgets/measure';
+import img1 from 'shared/assets/img/measure/tonometr.png';
+import img2 from 'shared/assets/img/measure/pulsococsimetr.png';
 
 export const CardioPage = () => {
 	const [isComplete, setIsComplete] = useState(false);
@@ -21,6 +22,18 @@ export const CardioPage = () => {
 			</PageHead>
 			<Steps current={3} />
 			<TitleBlock className={css['title']} title="Давайте измерим ваше давление и пульс" />
+			<div className={css['items']}>
+				<div className={css['item']}>
+					<h5 className={css['label']}>Вставьте руку в тонометр так, как показано на рисунке.</h5>
+					<img className={css['img']} src={img1} alt="image1" />
+				</div>
+				<div className={css['item']}>
+					<h5 className={css['label']}>
+						Вставьте указательный палец любой руки в пульсоксиметр, как показано на рисунке:
+					</h5>
+					<img className={css['img']} src={img2} alt="image2" />
+				</div>
+			</div>
 			<MeasureStatus isComplete={isComplete} />
 			<Measure action={startMeasure} onSuccess={() => setIsComplete(true)} nextStep={path.start()} />
 		</Container>
